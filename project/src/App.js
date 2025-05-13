96fd773a4d425ad46e59321a4fe36baf967e6bd0
Reda1mg <mohamed.elalaoui@viacesi.fr>
2025-05-13 11:24:24 +0200

    scoreboard working

--- project/src/App.js
+++ project/src/App.js
@@ -5,9 +5,10 @@ import Home from './pages/home/home';
 import Competitors from "./pages/Competitors/Competitors";
 import TournoiDetails from "./pages/TournoiDetails/ToutnoiDetails";
 import Telecommande from "./pages/Telecommande/components/Telecommande";
-import Scoreboard from "./pages/Scoreboard/Scoreboard";
+// import Scoreboard from "./pages/Scoreboard/Scoreboard";
 import AddCompetitorsToCategory from "./pages/TournoiDetails/Components/addCompetitorsToCategory";
 import MatchesTable from "./pages/Matches/Components/MatchesTable";
+import ScoreboardDynamic from "./pages/Scoreboard/components/ScoerBoardDynamic";
 
 function App() {
   return (
@@ -18,12 +19,11 @@ function App() {
           <Route path='/' element={<div className='content'><Home /></div>} />
           <Route path='/competiteurs' element={<div className='content'><Competitors /></div>} />
           <Route path='/telecommande' element={<div className='content'><Telecommande /></div>} />
-          <Route path='/scoreboard' element={<div className='content'><Scoreboard /></div>} />
           <Route path='/tournoiDetails/:id' element={<TournoiDetails />} />
           <Route path="/tournoiDetails/:id/ajouter-competiteurs" element={<AddCompetitorsToCategory />} />
           <Route path="/matches/:categoryId" element={<MatchesTable />} />
-          <Route path="/matches/:matchId/scoreboard" element={<Scoreboard />} />
           <Route path="/telecommande/:matchId" element={<Telecommande />} />
+          <Route path="/scoreboard/:matchId" element={<ScoreboardDynamic />} />
         </Routes>
       </Router>
     </div>
--- project/src/pages/home/home.js
+++ project/src/pages/home/home.js
@@ -1,5 +1,5 @@
-import Filters from "./Components/Filters";
-import TournoiTable from "./Components/TournamentTable";
+
+import TournoiTable from "./components/TournamentTable";
 import styles from "./home.module.css"
 
 function Home(){
