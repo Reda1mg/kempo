dfa2b8c05d71f11dc252b9fcadce35b4660f00e4
Kempo Dev <dev@kempo.local>
2025-07-16 10:24:01 +0200

    feat: add create tournament button in header

--- project/src/pages/home/home.js
+++ project/src/pages/home/home.js
@@ -1,13 +1,15 @@
 
 
 import TournoiTable from "./components/TournamentTable";
-
 import styles from "./home.module.css"
 
 function Home(){
     return(
     <div className="">
-      <h1 className = {styles.title}>Liste des Tournois</h1>
+      <div className={styles.header}>
+        <h1 className={styles.title}>Liste des Tournois</h1>
+        <button className={styles.createButton}>+ Créer un Tournoi</button>
+      </div>
       
       <TournoiTable />
 
